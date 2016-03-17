a509579936671a73131a724c2823b4a809ec5fa9
Kim Feinberg <kimberly380@gmail.com>
2016-03-16 18:22:19 -0700

    removed unnecessary files from branch and costmetic modifications to app2.js

--- app2.js
+++ app2.js
@@ -1,61 +1,52 @@
 
 //get to know user and send personalized message
-/*
 var userName = prompt("Hi. Welcome to my site! What's your name?");
 console.log("User name is " + userName);
-//document.getElementById("userName").innerText="Hi "+userName +"! Welcome to my site!";
 
-
-//introduce game and begin to display info on site if users initializes
-var readyToPlay = prompt("Hi " + userName + "!  Are you ready to play a Guessing Game (Y/N)?")
+//introduce game and ask for user participation.  If user refuses, no further questions will be asked.
+var readyToPlay = prompt("Hi " + userName + "!  Are you ready to play a Guessing Game about me (Y/N)?")
 var readyToPlayElement = document.getElementById("readyToPlay");
 
 if (readyToPlay.toUpperCase() === "N" || readyToPlay.toUpperCase() ==="NO") {
   console.log(userName + " refuses to play.");
   alert("Oh well, I guess you'll never know who I am.  Have a nice day!");
-  //readyToPlayElement.textContent="Oh well, I guess you'll never know who I am.  Have a nice day!";
+
     } else {
         console.log(userName + " is ready to play.");
-        //readyToPlayElement.textContent="Great! Let's Play!";
-        //document.getElementById("moreAboutMe").textContent="Here's some stuff about me..."
 
-        //ask first question and print answer results.
-        var questionOne = prompt("First Question: Am I originally from Portland?");
-        //var questionOneElement = document.getElementById("questionOne")
-        if (readyToPlay){
-            if (questionOne.toUpperCase() === "Y" || questionOne.toUpperCase() ==="YES") {
-              console.log('userName ' + "incorrectly said yes.");
-              alert("No, sorry that's incorrect. Let's move on.");
-              document.getElementById("questionOne").textContent="I am originally from Atlanta, but have lived in New York, Madrid and 6 years in Portland."
-            } else {
-              console.log('userName ' + "correctly said no.");
-              alert("Yes, that's correct! I am not from Portland.");
-            }
 
-            var questionTwo = prompt("Second Question: Do I love chocolate?");
-            //var questionOneElement = document.getElementById("questionOne")
+        //If user says they want to participate, then the following questions will be asked.
+          var questionOne = prompt("First Question: Am I originally from Portland?");
+
+          if (readyToPlay){
+              if (questionOne.toUpperCase() === "Y" || questionOne.toUpperCase() ==="YES") {
+                console.log(userName + "incorrectly said yes.");
+                alert("No, sorry that's incorrect. I'm not from Portland but have lived here for 6 years. Let's move on.");
+              } else {
+                console.log(userName  + "correctly said no.");
+                alert("Yes, that's correct! I'm actually from Atlanta. Next Question...");
+              }
+
+          var questionTwo = prompt("Second Question: Do I love chocolate?");
 
           if (questionTwo.toUpperCase() === "Y" || questionTwo.toUpperCase() === "YES") {
               console.log('userName ' + "incorrectly said yes.");
-              alert("Sorry, that's incorrect. Ok, one more question.");
-              document.getElementById("questionTwo").textContent="Crazy as it sounds, I actually don't like chocoloate. Must be a family thing."
-            } else {
+              alert("Sorry, wrong guess. I don't like chocolate much at all! Let's move on to the next question.");
+              } else {
               console.log('userName ' + "correctly said no.");
-              alert("That's correct!");
+              alert("That's correct! Not a fan of chocolate. Let's move on to the next question.");
             }
 
             var questionThree = prompt("Third Question: Can I run a 6 minute mile?");
 
           if (questionThree.toUpperCase() === "Y" || questionThree.toUpperCase() === "YES") {
               console.log('userName ' + "incorrectly said yes.");
-              alert("Sorry, that's incorrect. Let's move on to the last question.");
-              document.getElementById("questionThree").textContent="Crazy as it sounds, I actually don't like chocoloate. Must be a family thing."
-            } else {
+              alert("Of course I can't! Dont be silly! Just two more question to go...");
+              } else {
               console.log('userName ' + "correctly said no.");
-              alert("Oh heck no! Not even if being chased by wild monkeys! Way to Go, you got it right!");
+              alert("Of course I can't...you know me well.");
             }
 
-*/
           var myNum = 2;
           var keepRunning = true;
 
@@ -76,9 +67,5 @@ if (readyToPlay.toUpperCase() === "N" || readyToPlay.toUpperCase() ==="NO") {
             }
 
 
-/*
-          }
-
-
+          } //close of readyToPlay if statement
         }
-*/
